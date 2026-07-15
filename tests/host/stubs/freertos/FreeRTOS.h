f856d2de66fa8a1691d731bde6e19639cee991fa
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-07-15 09:20:25 +0200

    Add host scheduler yield support

--- tests/host/stubs/freertos/FreeRTOS.h
+++ tests/host/stubs/freertos/FreeRTOS.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <thread>
+
 using TickType_t = unsigned int;
 
 constexpr TickType_t portMAX_DELAY = 0xffffffffu;
 constexpr int pdTRUE = 1;
+constexpr int pdFALSE = 0;
+
+inline void taskYIELD() {
+	std::this_thread::yield();
+}
