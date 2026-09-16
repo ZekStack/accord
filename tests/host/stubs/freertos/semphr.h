9d90fafef742039f60d2348cb9289c58cd60324d
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-16 08:59:37 +0200

    Use static recursive mutexes in Accord host tests

--- tests/host/stubs/freertos/semphr.h
+++ tests/host/stubs/freertos/semphr.h
@@ -1,30 +1,39 @@
 #pragma once
 
 #include "FreeRTOS.h"
-#include <new>
 
+#include <memory>
 #include <mutex>
 
-using SemaphoreHandle_t = std::recursive_mutex *;
+struct StaticSemaphore_t {
+	std::recursive_mutex mutex;
+};
 
-inline SemaphoreHandle_t xSemaphoreCreateRecursiveMutex() {
-	return new (std::nothrow) std::recursive_mutex();
+using SemaphoreHandle_t = StaticSemaphore_t *;
+
+inline SemaphoreHandle_t xSemaphoreCreateRecursiveMutexStatic(StaticSemaphore_t *storage) {
+	if (storage == nullptr) {
+		return nullptr;
+	}
+	return std::construct_at(storage);
 }
 
 inline void vSemaphoreDelete(SemaphoreHandle_t handle) {
-	delete handle;
+	if (handle != nullptr) {
+		std::destroy_at(handle);
+	}
 }
 
 inline int xSemaphoreTakeRecursive(SemaphoreHandle_t handle, TickType_t) {
 	if (handle == nullptr) {
 		return pdFALSE;
 	}
-	handle->lock();
+	handle->mutex.lock();
 	return pdTRUE;
 }
 
 inline void xSemaphoreGiveRecursive(SemaphoreHandle_t handle) {
 	if (handle != nullptr) {
-		handle->unlock();
+		handle->mutex.unlock();
 	}
 }
